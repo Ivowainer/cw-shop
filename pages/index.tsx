eeb5571aacc2a10d3a65c814b7fcf9bfffb430e5
Ivowainer <ivancamposwainer@gmail.com>
2022-10-02 20:48:54 -0300

    All products withput real database

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
 import type { NextPage } from 'next'
-import { Typography } from '@mui/material'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
 
 import { ShopLayout } from '../components/layouts'
+import { initialData } from '../database/products';
+
+
 
 const Home: NextPage = () => {
   return (
     <ShopLayout title={'Home | CW Shop'} pageDescription={'Find the best products from CW Shop'}>
       <Typography variant="h1" component="h1">Tienda</Typography>
       <Typography variant="h6" sx={{ mb: 1 }}>All Products</Typography>
+
+      <Grid container spacing={4}>
+          {initialData.products.map(product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia 
+                    component="img"
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))}
+      </Grid>
     </ShopLayout>
   )
 }
