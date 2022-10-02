8b19c94b8235f0fc0524ed5284d907a36cbd8bcc
Ivowainer <ivancamposwainer@gmail.com>
2022-10-02 17:31:29 -0300

    Some styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { ThemeProvider } from '@emotion/react';
+import { lightTheme } from '../themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={}>
+    <ThemeProvider theme={lightTheme}>
       <Component {...pageProps} />
     </ThemeProvider>
   )
