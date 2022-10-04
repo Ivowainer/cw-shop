7b14452a2ed74d77212ed9092dd0dd73b557b63e
Ivowainer <ivancamposwainer@gmail.com>
2022-10-04 17:09:37 -0300

    Card Effect

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,4 +1,5 @@
-import { Card, CardActionArea, CardMedia, Grid } from "@mui/material"
+import { Box, Card, CardActionArea, CardMedia, Grid, Typography } from "@mui/material"
+import { useMemo, useState } from "react";
 import { IProducts } from "../../interfaces";
 
 interface ProductCardProps {
@@ -6,17 +7,34 @@ interface ProductCardProps {
 }
 
 export const ProductCard = ({ product }: ProductCardProps) => {
+  const [isHovered, setIsHovered] = useState(false) 
+
+  const productImage = useMemo(() => {
+    return isHovered ? `products/${product.images[1]}` : `products/${product.images[0]}`
+  }, [isHovered, product.images])
+
   return (
-    <Grid item xs={6} sm={4} key={product.slug}>
+    <Grid 
+      item 
+      xs={6} sm={4}
+      onMouseEnter={ () => setIsHovered(true) }
+      onMouseLeave={ () => setIsHovered(false) }
+    >
       <Card>
         <CardActionArea>
           <CardMedia 
             component="img"
-            image={`products/${product.images[0]}`}
+            className="fadeIn"
+            image={productImage}
             alt={product.title}
           />
         </CardActionArea>
       </Card>
+
+      <Box sx={{ mt: 1 }} className='fadeIn'>
+        <Typography fontWeight={700}>{ product.title }</Typography>
+        <Typography fontWeight={500}>{`$${product.price} `}</Typography>
+      </Box>
     </Grid>
   )
 }
