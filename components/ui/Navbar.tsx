3ef5de4e60efc8dd60368af9d76c124c244fdc23
Ivowainer <ivancamposwainer@gmail.com>
2022-10-12 11:19:10 -0300

    PathName Link

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,61 +1,64 @@
 import NextLink from 'next/link'
+import { useRouter } from 'next/router'
 
 import { Toolbar, AppBar, Typography, Link, Box, Button, IconButton, Badge } from '@mui/material'
 
 import { SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material'
 
 export const Navbar = () => {
-  return (
-    <AppBar position="fixed" color="primary">
-        <Toolbar>
-            <NextLink href="/" passHref>
-                <Link display="flex" alignItems="center">
-                    <Typography variant='h6'>CW |</Typography>
-                    <Typography sx={{ ml: 0.5 }}>Shop</Typography>
-                </Link>
-            </NextLink>
+    const { pathname } = useRouter()
 
-            <Box flex={ 1 } />
-
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-                <NextLink href="/category/men" passHref>
-                    <Link>
-                        <Button>Men</Button>
+    return (
+        <AppBar position="fixed" color="primary">
+            <Toolbar>
+                <NextLink href="/" passHref>
+                    <Link display="flex" alignItems="center">
+                        <Typography variant='h6'>CW |</Typography>
+                        <Typography sx={{ ml: 0.5 }}>Shop</Typography>
                     </Link>
                 </NextLink>
-                <NextLink href="/category/women" passHref>
+
+                <Box flex={ 1 } />
+
+                <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                    <NextLink href="/category/men" passHref>
+                        <Link>
+                            <Button color={pathname == '/category/men' ? 'primary' : 'info'}>Men</Button>
+                        </Link>
+                    </NextLink>
+                    <NextLink href="/category/women" passHref>
+                        <Link>
+                            <Button color={pathname == '/category/women' ? 'primary' : 'info'}>Women</Button>
+                        </Link>
+                    </NextLink>
+                    <NextLink href="/category/kid" passHref>
+                        <Link>
+                            <Button color={pathname == '/category/kid' ? 'primary' : 'info'}>Kid</Button>
+                        </Link>
+                    </NextLink>
+                </Box>
+
+                <Box flex={ 1 } />
+
+                <IconButton>
+                    <SearchOutlined />
+                </IconButton>
+
+                <NextLink href="/cart" passHref>
                     <Link>
-                        <Button>Women</Button>
+                        <IconButton>
+                            <Badge badgeContent={ 2 } color="secondary">
+                                <ShoppingCartOutlined />
+                            </Badge>
+                        </IconButton>
                     </Link>
                 </NextLink>
-                <NextLink href="/category/kid" passHref>
-                    <Link>
-                        <Button>Kid</Button>
-                    </Link>
-                </NextLink>
-            </Box>
 
-            <Box flex={ 1 } />
-
-            <IconButton>
-                <SearchOutlined />
-            </IconButton>
-
-            <NextLink href="/cart" passHref>
-                <Link>
-                    <IconButton>
-                        <Badge badgeContent={ 2 } color="secondary">
-                            <ShoppingCartOutlined />
-                        </Badge>
-                    </IconButton>
-                </Link>
-            </NextLink>
-
-            
-            <Button>
-                Menú
-            </Button>
-        </Toolbar>
-    </AppBar>
-  )
+                
+                <Button>
+                    Menú
+                </Button>
+            </Toolbar>
+        </AppBar>
+    )
 }
