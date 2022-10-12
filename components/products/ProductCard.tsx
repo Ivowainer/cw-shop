b74293fe3cc7dd780542da8029aff5ef96c8cf3c
Ivowainer <ivancamposwainer@gmail.com>
2022-10-12 10:47:15 -0300

    Text onLoad image

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -10,6 +10,7 @@ interface ProductCardProps {
 
 export const ProductCard = ({ product }: ProductCardProps) => {
   const [isHovered, setIsHovered] = useState(false) 
+  const [isImageLoaded, setIsImageLoaded] = useState(false) 
 
   const productImage = useMemo(() => {
     return isHovered ? `products/${product.images[1]}` : `products/${product.images[0]}`
@@ -31,13 +32,14 @@ export const ProductCard = ({ product }: ProductCardProps) => {
                 className="fadeIn"
                 image={productImage}
                 alt={product.title}
+                onLoad={() => setIsImageLoaded(true)}
               />
             </CardActionArea>
           </Link>
         </NextLink>
       </Card>
 
-      <Box sx={{ mt: 1 }} className='fadeIn'>
+      <Box sx={{ mt: 1, display: isImageLoaded ? 'block' : 'none' }} className='fadeIn'>
         <Typography fontWeight={700}>{ product.title }</Typography>
         <Typography fontWeight={500}>{`$${product.price} `}</Typography>
       </Box>
