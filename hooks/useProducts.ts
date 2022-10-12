81d1e0eaa48d0b2971c4596a32560b929a4300e3
Ivowainer <ivancamposwainer@gmail.com>
2022-10-12 10:34:48 -0300

    SWR Fetching Global

--- hooks/useProducts.ts
+++ hooks/useProducts.ts
@@ -1,14 +1,11 @@
 import useSWR, { SWRConfiguration } from 'swr'
 import { IProducts } from '../interfaces'
 
-const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
-
 export const useProducts = (url: string, config: SWRConfiguration = {}) => {
-
-    const { data, error } = useSWR<IProducts[]>(`/api${ url }`, fetcher, config)
+    const { data, error } = useSWR<IProducts[]>(`/api${ url }`, config)
 
     return {
-        products: data,
+        products: data || [],
         isLoading: !error && !data,
         isError: error
     }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import { SWRConfig } from 'swr'
+
 import { ThemeProvider } from '@mui/material/styles';
 import { lightTheme } from '../themes';
 
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SWRConfig 
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+      <ThemeProvider theme={lightTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   )
 }
 
