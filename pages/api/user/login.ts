edd3226d513367f725fbba1c90505162132c708a
Ivowainer <ivancamposwainer@gmail.com>
2022-11-02 16:12:18 -0300

    Create Login Post

---
+++ pages/api/user/login.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '../../../database'
+import { User } from '../../../models'
+import bcrypt from 'bcryptjs';
+
+type Data = 
+| { message: string } 
+| {
+    token: string;
+    user: {
+        email: string;
+        name: string;
+        role: string;
+    }
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+    switch(req.method) {
+        case 'POST':
+            return loginUser(req, res)
+
+        default:
+            res.status(400).json({ message: 'Bad Request' })
+    }
+}
+
+const loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const { email = '', password = '' } = req.body
+
+    await db.connect();
+    const user = await User.findOne({ email }).lean();
+    await db.disconnect();
+
+    if(!user) return res.status(400).json({ message: 'Invalid email or password - EMAIL ' })
+
+    if( !bcrypt.compareSync(password, user.password!) ) return res.status(400).json({ message: 'Invalid email or password - PASSWORD ' })
+
+    const { role, name } = user;
+
+    return res.status(200).json({ token: '', user: { email, role, name } })
+}
