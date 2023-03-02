419a0f5a97ae055ce0210c652d8fc43e7a6b8db5
Ivowainer <ivancamposwainer@gmail.com>
2023-03-02 12:04:02 -0300

    Address Form

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -1,47 +1,47 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { db } from '../../../database'
-import { User } from '../../../models'
-import bcrypt from 'bcryptjs';
-import { jwt } from '../../../utils';
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../database";
+import { User } from "../../../models";
+import bcrypt from "bcryptjs";
+import { jwt } from "../../../utils";
 
-type Data = 
-| { message: string } 
-| {
-    token: string;
-    user: {
-        email: string;
-        name: string;
-        role: string;
-    }
-}
+type Data =
+    | { message: string }
+    | {
+          token: string;
+          user: {
+              email: string;
+              name: string;
+              role: string;
+          };
+      };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    switch(req.method) {
-        case 'POST':
-            return loginUser(req, res)
+    switch (req.method) {
+        case "POST":
+            return loginUser(req, res);
 
         default:
-            res.status(400).json({ message: 'Bad Request' })
+            res.status(400).json({ message: "Bad Request" });
     }
 }
 
 const loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-    const { email = '', password = '' } = req.body
+    const { email = "", password = "" } = req.body;
 
     await db.connect();
     const user = await User.findOne({ email }).lean();
     await db.disconnect();
 
-    if(!user) return res.status(400).json({ message: 'Invalid email or password - EMAIL ' })
+    if (!user) return res.status(400).json({ message: "Invalid email or password - EMAIL " });
 
-    if( !bcrypt.compareSync(password, user.password!) ) return res.status(400).json({ message: 'Invalid email or password - PASSWORD ' })
+    if (!bcrypt.compareSync(password, user.password!)) return res.status(400).json({ message: "Invalid email or password - PASSWORD " });
 
     const { role, name, _id } = user;
 
-    const token = jwt.signToken( _id, email )
+    const token = jwt.signToken(_id, email);
 
-    return res.status(200).json({ 
-        token, 
-        user: { email, role, name }
-    })
-}
+    return res.status(200).json({
+        token,
+        user: { email, role, name },
+    });
+};
--- pages/checkout/address.tsx
+++ pages/checkout/address.tsx
@@ -1,56 +1,163 @@
-import { GetServerSideProps } from "next";
+import { countries, validations } from "../../utils";
 
-import { jwt } from "../../utils";
+import { useForm } from "react-hook-form";
 
 import { Box, Button, FormControl, Grid, MenuItem, Select, TextField, Typography } from "@mui/material";
 import { ShopLayout } from "../../components/layouts";
+import Cookies from "js-cookie";
+
+type FormData = {
+    name: string;
+    firstName: string;
+    lastName: string;
+    address: string;
+    address2: string;
+    zipCode: string;
+    city: string;
+    country: string;
+    phone: string;
+};
 
 const AddressPage = () => {
+    //prettier-ignore
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
+        defaultValues: {
+            firstName: '',
+            lastName: '',
+            address: '',
+            address2: '',
+            zipCode: '',
+            city: '',
+            country: countries[0].code,
+            phone: '',
+        }
+    });
+
+    const onReviewOrder = (data: FormData) => {
+        console.log(data);
+
+        Cookies.set("firstName", data.firstName);
+        Cookies.set("lastName", data.lastName);
+        Cookies.set("address", data.address);
+        Cookies.set("address2", data.address2 || "");
+        Cookies.set("zipCode", data.zipCode);
+        Cookies.set("city", data.city);
+        Cookies.set("country", data.country);
+        Cookies.set("phone", data.phone);
+    };
+
     return (
         <ShopLayout title={"Address | CW Shop"} pageDescription={"Confirm direction"}>
             <Typography variant="h1" component="h1">
                 Direction
             </Typography>
 
-            <Grid container spacing={2} sx={{ mt: 2 }}>
-                <Grid item xs={12} sm={6}>
-                    <TextField label="Name" variant="filled" fullWidth />
+            <form onSubmit={handleSubmit(onReviewOrder)}>
+                <Grid container spacing={2} sx={{ mt: 2 }}>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label="Name"
+                            variant="filled"
+                            fullWidth
+                            {...register("firstName", {
+                                required: "This field is required",
+                            })}
+                            error={!!errors.firstName}
+                            helperText={errors.firstName?.message}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label="Last Name"
+                            variant="filled"
+                            fullWidth
+                            {...register("lastName", {
+                                required: "This field is required",
+                            })}
+                            error={!!errors.lastName}
+                            helperText={errors.lastName?.message}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label="Direction"
+                            variant="filled"
+                            fullWidth
+                            {...register("address", {
+                                required: "This field is required",
+                            })}
+                            error={!!errors.address}
+                            helperText={errors.address?.message}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField label="Direction 2 (optional)" variant="filled" fullWidth {...register("address2")} error={!!errors.address2} helperText={errors.address2?.message} />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label="Postal Code"
+                            variant="filled"
+                            fullWidth
+                            {...register("zipCode", {
+                                required: "This field is required",
+                            })}
+                            error={!!errors.zipCode}
+                            helperText={errors.zipCode?.message}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label="City"
+                            variant="filled"
+                            fullWidth
+                            {...register("city", {
+                                required: "This field is required",
+                            })}
+                            error={!!errors.city}
+                            helperText={errors.city?.message}
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <FormControl fullWidth>
+                            <TextField
+                                select
+                                variant="filled"
+                                label="Country"
+                                defaultValue={countries[0].code}
+                                {...register("country", {
+                                    required: "This field is required",
+                                })}
+                                error={!!errors.city}
+                                helperText={errors.city?.message}
+                            >
+                                {countries.map((country, i) => (
+                                    <MenuItem key={country.code} value={country.code}>
+                                        {country.name}
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <TextField
+                            label="Phone"
+                            variant="filled"
+                            fullWidth
+                            {...register("phone", {
+                                required: "This field is required",
+                            })}
+                            error={!!errors.phone}
+                            helperText={errors.phone?.message}
+                        />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField label="Last Name" variant="filled" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField label="Direction" variant="filled" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField label="Direction 2 (optional)" variant="filled" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField label="Postal Code" variant="filled" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField label="City" variant="filled" fullWidth />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <FormControl fullWidth>
-                        <Select variant="filled" label="Country" value={1}>
-                            <MenuItem value={1}>Argentina</MenuItem>
-                            <MenuItem value={2}>Chile</MenuItem>
-                            <MenuItem value={3}>Honduras</MenuItem>
-                            <MenuItem value={4}>México</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <TextField label="Phone" variant="filled" fullWidth />
-                </Grid>
-            </Grid>
 
-            <Box sx={{ mt: 5 }} display="flex" justifyContent="center">
-                <Button color="secondary" className="circular-btn" size="large">
-                    Review order
-                </Button>
-            </Box>
+                <Box sx={{ mt: 5 }} display="flex" justifyContent="center">
+                    <Button type="submit" color="secondary" className="circular-btn" size="large">
+                        Review order
+                    </Button>
+                </Box>
+            </form>
         </ShopLayout>
     );
 };
